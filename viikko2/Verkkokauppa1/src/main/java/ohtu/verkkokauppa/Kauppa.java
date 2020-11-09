7c7e823184c1150bddde11c3df70c6cedfaacff5
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-11-09 19:18:07 +0200

    9. riippuvuuksien injektointi osa 3: Verkkokauppa

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -0,0 +1,46 @@
+package ohtu.verkkokauppa;
+
+public class Kauppa {
+
+    private VarastoInterface varasto;
+    private PankkiInterface pankki;
+    private Ostoskori ostoskori;
+    private ViitegeneraattoriInterface viitegeneraattori;
+    private String kaupanTili;
+
+    public Kauppa(VarastoInterface varastoIF, PankkiInterface pankkiIF
+            , ViitegeneraattoriInterface viitegeneraattoriIF) {
+        varasto = varastoIF;
+        pankki = pankkiIF;
+        viitegeneraattori = viitegeneraattoriIF;
+        kaupanTili = "33333-44455";
+    }
+
+    public void aloitaAsiointi() {
+        ostoskori = new Ostoskori();
+    }
+
+    public void poistaKorista(int id) {
+        Tuote t = varasto.haeTuote(id);
+        varasto.palautaVarastoon(t);
+    }
+
+    public void lisaaKoriin(int id) {
+//        System.out.println("Ollaanko täällä?");
+//        System.out.println("varasto: " + varasto);
+        
+        if (varasto.saldo(id) > 0) {
+            Tuote t = varasto.haeTuote(id);
+            ostoskori.lisaa(t);
+            varasto.otaVarastosta(t);
+        }
+    }
+
+    public boolean tilimaksu(String nimi, String tiliNumero) {
+        int viite = viitegeneraattori.uusi();
+        int summa = ostoskori.hinta();
+
+        return pankki.tilisiirto(nimi, viite, tiliNumero, kaupanTili, summa);
+    }
+
+}
