46c47e474ae6a699bc329aaf94751b1c6492345d
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-11-15 14:57:16 +0200

    1. lisää gradlea: riippuvuuksien lisääminen

---
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -0,0 +1,79 @@
+
+package ohtu;
+
+public class Player {
+    
+    /*
+    {"name":"CJ Suess","nationality":"USA","assists":0,"goals":0,
+    "penalties":0,"team":"WPG","games":1}
+    */
+    private String name;
+    private String nationality;
+    private Integer assists;
+    private Integer goals;
+    private Integer penalties;
+    private String team;
+    private Integer games;
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public Integer getAssists() {
+        return assists;
+    }
+
+    public void setAssists(Integer assists) {
+        this.assists = assists;
+    }
+
+    public Integer getGoals() {
+        return goals;
+    }
+
+    public void setGoals(Integer goals) {
+        this.goals = goals;
+    }
+
+    public Integer getPenalties() {
+        return penalties;
+    }
+
+    public void setPenalties(Integer penalties) {
+        this.penalties = penalties;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public Integer getGames() {
+        return games;
+    }
+
+    public void setGames(Integer games) {
+        this.games = games;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " team " + team + " goals " + goals + " assists " + assists;
+    }
+      
+}
