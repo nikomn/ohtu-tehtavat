1eff2ad621513239f470c94a66289af4f4129726
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-11-23 09:27:07 +0200

    4. Yksikkötestaus ja riippuvuudet: Mockito, osa 4

--- viikko4/Verkkokauppa1/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4/Verkkokauppa1/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -6,30 +6,29 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class KauppaTest {
-    
+
     Pankki pankki;
     Viitegeneraattori viite;
     Varasto varasto;
     Kauppa k;
-    
+
     @Before
     public void setUp() {
         pankki = mock(Pankki.class);
         viite = mock(Viitegeneraattori.class);
         varasto = mock(Varasto.class);
         k = new Kauppa(varasto, pankki, viite);
-        
+
         when(viite.uusi()).thenReturn(42);
-        
 
         // tuote numero 1 on maito jonka hinta on 5 ja saldo 10
         when(varasto.saldo(1)).thenReturn(10);
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
-        
+
         // tuote numero 2 on leipä jonka hinta on 4 ja saldo 15
         when(varasto.saldo(2)).thenReturn(15);
         when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "leipä", 4));
-        
+
         // tuote numero 3 on piimä jonka hinta on 6 ja saldo 0
         when(varasto.saldo(3)).thenReturn(0);
         when(varasto.haeTuote(3)).thenReturn(new Tuote(3, "piimä", 6));
@@ -46,9 +45,7 @@ public class KauppaTest {
         verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(), anyInt());
         // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
     }
-    
-    
-    
+
     @Test
     public void ostoksenPaaytyttyaPankkistaOnTehtyOikeaTilisiirto() {
         // tehdään ostokset
@@ -60,7 +57,7 @@ public class KauppaTest {
         // pankki.tilisiirto(nimi, viite, tiliNumero, kaupanTili, summa);
         verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(5));
     }
-    
+
     @Test
     public void kahdenEriTuotteenOstamisenJalkeenPankkistaOnTehtyOikeaTilisiirto() {
         k.aloitaAsiointi();
@@ -70,7 +67,7 @@ public class KauppaTest {
 
         verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(9));
     }
-    
+
     @Test
     public void kahdenSamanTuotteenOstamisenJalkeenPankkistaOnTehtyOikeaTilisiirto() {
         k.aloitaAsiointi();
@@ -80,7 +77,7 @@ public class KauppaTest {
 
         verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(10));
     }
-    
+
     @Test
     public void varastossaOlevanYhdenJaToisenLoppuneenOstamisenJalkeenPankkistaOnTehtyOikeaTilisiirto() {
         k.aloitaAsiointi();
@@ -89,6 +86,42 @@ public class KauppaTest {
         k.tilimaksu("pekka", "12345");
 
         verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(5));
+
+    }
+
+    @Test
+    public void asioinninAloittaminenNollaaEdellisetOstokset() {
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+        k.aloitaAsiointi();
+        k.lisaaKoriin(2);
+        k.tilimaksu("timo", "21345");
+        verify(pankki).tilisiirto(eq("timo"), eq(42), eq("21345"), anyString(), eq(4));
         
+    }
+    
+    @Test
+    public void kauppaPyytaaUudenViitenumeronJokaiselleOstokselle() {
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+        k.aloitaAsiointi();
+        k.lisaaKoriin(2);
+        k.tilimaksu("timo", "21345");
+        k.aloitaAsiointi();
+        k.lisaaKoriin(2);
+        k.tilimaksu("jaakko", "21354");
+        verify(viite, times(3)).uusi();
+    }
+    
+    @Test
+    public void tuotteenPoistaminenKoristaToimii() {
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.poistaKorista(1);
+        k.lisaaKoriin(2);
+        k.tilimaksu("pekka", "12345");
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(4));
     }
 }
