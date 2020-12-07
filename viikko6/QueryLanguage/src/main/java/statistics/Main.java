c61bf6935bd7c99f7a3768ede6bd55c83637d9c9
Niko Nieminen <niko.m.nieminen@helsinki.fi>
2020-12-07 08:57:50 +0200

    3. Kyselykieli NHLStatistics-ohjelmaan, osa 2

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -17,16 +17,27 @@ public class Main {
 //                new Not(new HasAtLeast(1, "goals")),
 //                new PlaysIn("NYR")
 //        );
+//        Matcher m = new And(
+//                new HasFewerThan(1, "goals"),
+//                new PlaysIn("NYR")
+//        );
+//        Matcher m = new Or(new HasAtLeast(40, "goals"),
+//                new HasAtLeast(60, "assists")
+//        );
         Matcher m = new And(
-                new HasFewerThan(1, "goals"),
-                new PlaysIn("NYR")
+                new HasAtLeast(50, "points"),
+                new Or(
+                        new PlaysIn("NYR"),
+                        new PlaysIn("NYI"),
+                        new PlaysIn("BOS")
+                )
         );
 
         for (Player player : stats.matches(m)) {
             System.out.println(player);
         }
-        
-        System.out.println(stats.matches(new All()).size());
+
+        //System.out.println(stats.matches(new All()).size());
 
     }
 }
